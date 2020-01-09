6e8ec5a2e818a17f3c3dc4dc6ba1035cd8144327
Travis <traviseengland565@gmail.com>
2020-01-09 00:01:35 -0500

    Updating django web app

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,3 +10,19 @@ class Topic(models.Model):
     def __str__(self):
         #return string representation of models
         return self.text
+
+class Entry(models.Model):
+    #Something specific learned about a topicself.
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        #Return a string representation of the modelself.
+        if(len(self.text) > 50):
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text[:50]}"
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    #Home page for learning blog
+    return render(request, 'learning_logs/index.html')
